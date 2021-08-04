f923f4f3d2186b07aeafe529e11fddc8a3ad62d2
Ashwin-Developer <88386942+Ashwin-Developer@users.noreply.github.com>
2021-08-04 18:39:21 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,22 @@
+//1st assignment
+
+a = ["Ironman","Captain America","Hulk","Thor","Loki"];
+i=4;
+while(i<=4){
+  console.log(a[i]);
+  i = i-1;
+  if(i<0){
+    break;
+  }
+}
+
+//2nd assignment
+
+for(i=0;i<=50;i++){
+  
+    if(i%5 == 0){
+      console.log(i);
+    }
+  }
+  
+
